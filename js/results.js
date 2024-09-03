552ece9798ba66e6bb8ed984da0e6618094783ef
Belial_Alu <127793189+BelialALu@users.noreply.github.com>
2024-09-03 22:06:57 +0300

    Update results.js

--- js/results.js
+++ js/results.js
@@ -1,27 +1,33 @@
 document.addEventListener('DOMContentLoaded', displayResults);
 
+(function () {
+    emailjs.init("Q5mH9WW5IYLU5Qlm1");  // Замените YOUR_PUBLIC_API_KEY на ваш Public API Key
+})();
+
 const correctAnswers = {
-    "1": "17",
-    "2": "3x-2xy-2y",
-    "3": "-3",
-    "4": "24/35,5/7,11/14,129/140",
-    "5": "27",
-    "6": "(3,3)",
-    "7": "-1",
+    "1": "10",
+    "2": "680",
+    "3": "Ethernet",
+    "4": "фиолетовый",
+    "5": "A2B6",
+    "6": "алгоритм",
+    "7": "1111101",
 };
 
-function displayResults() {
-    const answers = JSON.parse(localStorage.getItem('quizAnswers_informatics'));
-    if (!answers) {
-        document.getElementById('results').innerHTML = '<p>Ответы не найдены. Пожалуйста, пройдите тест.</p>';
-        return;
-    }
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
 
+function displayResults() {
+    const subject = getQueryParam('subject');
+    const answers = JSON.parse(localStorage.getItem(`quizAnswers_${subject}`));
+    const correctAnswersForSubject = correctAnswers[subject];
     let correctCount = 0;
     let resultsHtml = '';
 
     for (const [question, answer] of Object.entries(answers)) {
-        const correctAnswer = correctAnswers[question];
+        const correctAnswer = correctAnswersForSubject[question];
         if (answer === correctAnswer) {
             correctCount++;
         }
@@ -42,7 +48,8 @@ function closeEmailModal() {
 
 function sendEmail() {
     const email = document.getElementById('emailInput').value;
-    const incorrectAnswers = getIncorrectAnswers();  // Функция для получения неверных ответов
+    const subject = getQueryParam('subject');
+    const incorrectAnswers = getIncorrectAnswers(subject);
     if (email) {
         emailjs.send("service_nj9r4m3", "template_fio3l8v", {
             to_email: email,
@@ -59,12 +66,13 @@ function sendEmail() {
     }
 }
 
-function getIncorrectAnswers() {
-    const answers = JSON.parse(localStorage.getItem('quizAnswers_informatics'));
+function getIncorrectAnswers(subject) {
+    const answers = JSON.parse(localStorage.getItem(`quizAnswers_${subject}`));
     const incorrectAnswers = [];
+    const correctAnswersForSubject = correctAnswers[subject];
     for (const [question, answer] of Object.entries(answers)) {
-        if (correctAnswers[question] !== answer) {
-            incorrectAnswers.push(`Вопрос ${question}: Ваш ответ "${answer}", Правильный ответ "${correctAnswers[question]}"`);
+        if (correctAnswersForSubject[question] !== answer) {
+            incorrectAnswers.push(`Вопрос ${question}: Ваш ответ "${answer}", Правильный ответ "${correctAnswersForSubject[question]}"`);
         }
     }
     return incorrectAnswers;
