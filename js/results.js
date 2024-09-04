1455165e4a0b778fc973a460e03eb1f7e04dee50
Belial_Alu <127793189+BelialALu@users.noreply.github.com>
2024-09-04 16:30:54 +0300

    Update results.js

--- js/results.js
+++ js/results.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', displayResults);
 const correctAnswers = {
     "1": "10",
     "2": "680",
-  	"3": "Ethernet",
-   	"4": "фиолетовый",
+    "3": "Ethernet",
+    "4": "фиолетовый",
     "5": "A2B6",
     "6": "алгоритм",
     "7": "1111101",
@@ -32,40 +32,4 @@ function displayResults() {
     document.getElementById('results').innerHTML = resultsHtml;
 }
 
-function openEmailModal() {
-    document.getElementById('emailModal').style.display = 'block';
-}
-
-function closeEmailModal() {
-    document.getElementById('emailModal').style.display = 'none';
-}
-
-function sendEmail() {
-    const email = document.getElementById('emailInput').value;
-    const incorrectAnswers = getIncorrectAnswers();  // Функция для получения неверных ответов
-    if (email) {
-        emailjs.send("service_nj9r4m3", "template_fio3l8v", {
-            to_email: email,
-            incorrect_answers: incorrectAnswers.join("\n")
-        })
-        .then(function (response) {
-            alert('Письмо успешно отправлено!');
-            closeEmailModal();
-        }, function (error) {
-            alert('Ошибка при отправке письма: ' + JSON.stringify(error));
-        });
-    } else {
-        alert('Пожалуйста, введите корректный email.');
-    }
-}
-
-function getIncorrectAnswers() {
-    const answers = JSON.parse(localStorage.getItem('quizAnswers_informatics'));
-    const incorrectAnswers = [];
-    for (const [question, answer] of Object.entries(answers)) {
-        if (correctAnswers[question] !== answer) {
-            incorrectAnswers.push(`Вопрос ${question}: Ваш ответ "${answer}", Правильный ответ "${correctAnswers[question]}"`);
-        }
-    }
-    return incorrectAnswers;
-}
+// Удалены функции openEmailModal, closeEmailModal, sendEmail, getIncorrectAnswers
